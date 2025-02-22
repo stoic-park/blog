6cffe45fbe97092a6844aab4b5f3ffb233002d6c
sungtaekpark <sungtaek.park@onepredict.com>
2025-02-22 16:34:30 +0900

    feat: dark mode control

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  // dark mode 수동으로 지정하기!
+  // class 명을 바꿀 수 있도록 해주면 dark mode 컨트롤을 할 수 있다.
   darkMode: 'selector',
   content: ['./(app|src)/**/*.{js,ts,jsx,tsx,mdx}'],
 }
