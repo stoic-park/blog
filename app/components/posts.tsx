87e829f110c39044aec1b5fa05f24b3121df33ab
sungtaekpark <sungtaek.park@onepredict.com>
2025-03-07 03:34:11 +0900

    fix: layout

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -18,11 +18,11 @@ export function BlogPosts() {
         .map((post) => (
           <Link
             key={post.slug}
-            className="flex flex-col space-y-1 mb-4 hover:bg-neutral-100 dark:hover:bg-neutral-800 rounded-lg p-2 transition-colors"
+            className="flex flex-col space-y-1 mb-4 hover:bg-neutral-100 dark:hover:bg-neutral-800 rounded-lg p-2 px-4 transition-colors"
             href={`/post/${post.slug}`}
           >
             <div className="w-full flex flex-row items-center">
-              <p className="text-neutral-600 dark:text-neutral-400 w-50 shrink-0 tabular-nums mr-4">
+              <p className="text-neutral-600 dark:text-neutral-400 w-48 shrink-0 tabular-nums mr-4">
                 {formatDate(post.metadata.publishedAt, false)}
               </p>
               <p className="text-neutral-900 dark:text-neutral-100 tracking-tight group-hover:text-neutral-800">
