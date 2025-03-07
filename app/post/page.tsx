aefce1ab9981758357287c17e6ce1d3866371435
sungtaekpark <sungtaek.park@onepredict.com>
2025-03-07 17:55:43 +0900

    fix: tag 변경시 리랜더링 이슈 exit

--- app/post/page.tsx
+++ app/post/page.tsx
@@ -1,5 +1,6 @@
 import { BlogPosts } from 'app/components/posts'
 import { getBlogPosts } from './utils'
+import Link from 'next/link'
 
 export const metadata = {
   title: 'Post',
@@ -20,8 +21,18 @@ export default function Page({
     <section>
       <h1 className="font-semibold text-2xl mb-8 tracking-tighter">Post</h1>
       <div className="flex gap-2 flex-wrap mb-8">
+        <Link
+          href="/post"
+          className={`text-sm rounded-sm px-3 py-0.5 transition-colors ${
+            !searchParams.tag
+              ? 'bg-neutral-800 dark:bg-neutral-100 text-neutral-100 dark:text-neutral-800'
+              : 'bg-neutral-100 dark:bg-neutral-800 text-neutral-800 dark:text-neutral-100 hover:bg-neutral-200 dark:hover:bg-neutral-700'
+          }`}
+        >
+          All
+        </Link>
         {allTags.map((tag) => (
-          <a
+          <Link
             key={tag}
             href={searchParams.tag === tag ? '/post' : `/post?tag=${tag}`}
             className={`text-sm rounded-sm px-3 py-0.5 transition-colors ${
@@ -31,7 +42,7 @@ export default function Page({
             }`}
           >
             {tag}
-          </a>
+          </Link>
         ))}
       </div>
       <div className="my-8">
