1d12bc090cab25fb8bd047ecda1fcd5a126b33a1
stoic-park <demige79@gmail.com>
2025-03-30 17:48:10 +0900

    fix: layout meta 정보 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,67 +10,84 @@ import { baseUrl } from './sitemap'
 import { themeControl } from '../utils/themeControl'
 
 export const metadata: Metadata = {
-  metadataBase: new URL(baseUrl),
-  title: {
-    default: 'blog, stoic-park',
-    template: '%s | blog, stoic-park',
+ metadataBase: new URL(baseUrl),
+ title: {
+  default: 'Stoic Park - 개발 블로그',
+  template: '%s | Stoic Park Blog',
+ },
+ description:
+  'Stoic Park의 개발 블로그입니다. 웹 개발, JavaScript, React, Next.js 관련 기술 포스트를 공유합니다.',
+ openGraph: {
+  title: 'Stoic Park - 개발 블로그',
+  description:
+   'Stoic Park의 개발 블로그입니다. 웹 개발, JavaScript, React, Next.js 관련 기술 포스트를 공유합니다.',
+  url: baseUrl,
+  siteName: 'Stoic Park Blog',
+  locale: 'ko_KR',
+  type: 'website',
+  images: [
+   {
+    url: `${baseUrl}/og-image.png`,
+    width: 1200,
+    height: 630,
+   },
+  ],
+ },
+ robots: {
+  index: true,
+  follow: true,
+  googleBot: {
+   index: true,
+   follow: true,
+   'max-video-preview': -1,
+   'max-image-preview': 'large',
+   'max-snippet': -1,
   },
-  description: 'blog, stoic-park.',
-  openGraph: {
-    title: 'blog, stoic-park',
-    description: 'blog, stoic-park',
-    url: baseUrl,
-    siteName: 'stoic-park blog',
-    locale: 'en_US',
-    type: 'website',
-  },
-  robots: {
-    index: true,
-    follow: true,
-    googleBot: {
-      index: true,
-      follow: true,
-      'max-video-preview': -1,
-      'max-image-preview': 'large',
-      'max-snippet': -1,
-    },
-  },
+ },
+ keywords: [
+  'Stoic Park',
+  '개발 블로그',
+  '웹 개발',
+  'JavaScript',
+  'React',
+  'Next.js',
+ ],
 }
 
 const cx = (...classes) => classes.filter(Boolean).join(' ')
 
 export default function RootLayout({
-  children,
+ children,
 }: {
-  children: React.ReactNode
+ children: React.ReactNode
 }) {
-  return (
-    <html
-      lang="en"
-      className={cx(
-        'text-black bg-white dark:text-white dark:bg-black h-full',
-        GeistSans.variable,
-        GeistMono.variable,
-      )}
-    >
-      <head>
-        <script
-          dangerouslySetInnerHTML={{ __html: `(${themeControl.toString()})()` }}
-        />
-      </head>
-      <body className="antialiased min-h-screen max-w-3xl mx-4 lg:mx-auto flex flex-col h-full">
-        <main className="flex flex-col flex-1 px-2 md:px-0">
-          <div className="relative w-full h-12 mb-8">
-            <div className="absolute inset-0 blur-xl opacity-50"></div>
-            <div className="relative z-10 w-full h-full"></div>
-          </div>
-          <Navbar />
-          <div className="flex-grow">{children}</div>
-          <Footer />
-          <Analytics />
-          <SpeedInsights />
-        </main>
-      </body>
-    </html>
-  )
+ return (
+  <html
+   lang="en"
+   className={cx(
+    'text-black bg-white dark:text-white dark:bg-black h-full',
+    GeistSans.variable,
+    GeistMono.variable,
+   )}
+  >
+   <head>
+    <script
+     dangerouslySetInnerHTML={{ __html: `(${themeControl.toString()})()` }}
+    />
+   </head>
+   <body className="antialiased min-h-screen max-w-3xl mx-4 lg:mx-auto flex flex-col h-full">
+    <main className="flex flex-col flex-1 px-2 md:px-0">
+     <div className="relative w-full h-12 mb-8">
+      <div className="absolute inset-0 blur-xl opacity-50"></div>
+      <div className="relative z-10 w-full h-full"></div>
+     </div>
+     <Navbar />
+     <div className="flex-grow">{children}</div>
+     <Footer />
+     <Analytics />
+     <SpeedInsights />
+    </main>
+   </body>
+  </html>
+ )
 }
