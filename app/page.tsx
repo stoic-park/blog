de308982a7134365a30941c52150f50b999583d2
stoic-park <demige79@gmail.com>
2025-04-13 22:15:01 +0900

    post: 항해플러스 3주차 WIL

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,13 @@
 import { BlogPosts } from 'app/components/posts'
 //TODO: home -> post로 변경, home 삭제!
 export default function Page() {
-  return (
-    <section>
-      {/* <p>4년차, 코드와 사람을 잇는 프론트엔드 개발자 박성택입니다.</p>
-      <br />
-      <p>
-        단순한 개발을 넘어, 함께하는 사람들과 소통을 중심으로 보다 나은 결과를
-        향해 나아가고 싶습니다
-      </p> */}
-      <div className="my-8">
-        <div className="mb-2">최신 포스트</div>
-        <BlogPosts />
-      </div>
-    </section>
-  )
+ return (
+  <section>
+   <div>
+    {/* <div className="mb-8">최신 포스트</div> */}
+    <h1 className="font-semibold text-2xl mb-8 tracking-tighter">Latest</h1>
+    <BlogPosts />
+   </div>
+  </section>
+ )
 }
