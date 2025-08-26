9b6f18247d765201412166d38be26361ba64a2ce
stoic-park <demige79@gmail.com>
2025-08-26 21:20:04 +0900

    style: callout 컴포넌트 텍스트 컬러 옵션 추가

--- app/components/CustomComponents/Callout.tsx
+++ app/components/CustomComponents/Callout.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react'
 
 export const TipCallout = ({ children }: { children: ReactNode }) => {
  return (
-  <div className="bg-green-300/20 dark:bg-green-700/30 border-l-4 border-green-500 rounded-md px-5 pb-5 mb-5">
+  <div className="bg-green-300/20 dark:bg-green-700/30 border-l-4 border-green-500 rounded-md px-5 pb-5 mb-5 text-neutral-800 dark:text-neutral-200">
    <div className="flex items-center pt-5">
     <span className="font-extrabold">Tip</span>
    </div>
@@ -13,7 +13,7 @@ export const TipCallout = ({ children }: { children: ReactNode }) => {
 
 export const NoteCallout = ({ children }: { children: ReactNode }) => {
  return (
-  <div className="bg-blue-300/20 dark:bg-blue-700/30 border-l-4 border-blue-500 rounded-md px-5 pb-5 mb-5">
+  <div className="bg-blue-300/20 dark:bg-blue-700/30 border-l-4 border-blue-500 rounded-md px-5 pb-5 mb-5 text-neutral-800 dark:text-neutral-200">
    <div className="flex items-center pt-5">
     <span className="font-extrabold">Note</span>
    </div>
@@ -24,7 +24,7 @@ export const NoteCallout = ({ children }: { children: ReactNode }) => {
 
 export const WarningCallout = ({ children }: { children: ReactNode }) => {
  return (
-  <div className="bg-yellow-300/20 dark:bg-yellow-700/30 border-l-4 border-yellow-500 rounded-md px-5 pb-5 mb-5">
+  <div className="bg-yellow-300/20 dark:bg-yellow-700/30 border-l-4 border-yellow-500 rounded-md px-5 pb-5 mb-5 text-neutral-800 dark:text-neutral-200">
    <div className="flex items-center pt-5">
     <span className="font-extrabold">Warning</span>
    </div>
@@ -35,7 +35,7 @@ export const WarningCallout = ({ children }: { children: ReactNode }) => {
 
 export const ErrorCallout = ({ children }: { children: ReactNode }) => {
  return (
-  <div className="bg-red-300/20 dark:bg-red-700/30 border-l-4 border-red-500 rounded-md px-5 pb-5 mb-5">
+  <div className="bg-red-300/20 dark:bg-red-700/30 border-l-4 border-red-500 rounded-md px-5 pb-5 mb-5 text-neutral-800 dark:text-neutral-200">
    <div className="flex items-center pt-5">
     <span className="font-extrabold">Error</span>
    </div>
@@ -46,7 +46,7 @@ export const ErrorCallout = ({ children }: { children: ReactNode }) => {
 
 export const InfoCallout = ({ children }: { children: ReactNode }) => {
  return (
-  <div className="bg-indigo-300/20 dark:bg-indigo-700/30 border-l-4 border-indigo-500 rounded-md px-5 pb-5 mb-5">
+  <div className="bg-indigo-300/20 dark:bg-indigo-700/30 border-l-4 border-indigo-500 rounded-md px-5 pb-5 mb-5 text-neutral-800 dark:text-neutral-200">
    <div className="flex items-center pt-5">
     <span className="font-extrabold">Info</span>
    </div>
